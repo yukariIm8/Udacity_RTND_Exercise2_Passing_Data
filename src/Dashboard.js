b0f07cfbe779896506d2b822dce7ea26e493b69d
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-09-14 23:39:58 -0700

    feat: display display a list of all of the movies, and each movie has a list of users.

---
+++ src/Dashboard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+/* display a list of all of the movies, and each movie has a list of users who said that
+   this movie was their favorite along with the title of the movie. */
+class Dashboard extends Component {
+	render() {
+    	const { usersByMovie, users, movies } = this.props;
+      
+      	const movieCards = Object.key(movies).map(id => (
+        	<MovieCard 
+             	key={id}
+  				users={users}
+				usersWhoLikesMovie={usersByMovie[id]}
+				movieInfo={movies[id]}
+			/>
+        ));
+
+		return <ul>{movieCards}</ul>      
+    }
+}
+
+export default Dashboard;
